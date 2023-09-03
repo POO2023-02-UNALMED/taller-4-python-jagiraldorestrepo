b5460963a7a2c76c14bf2ac87ba4d728fc190b51
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-02 21:15:46 -0500

    --

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas = [], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
@@ -16,8 +16,8 @@ class Grupo:
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
