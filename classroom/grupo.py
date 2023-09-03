479752ededcaec7d7b913ca5abb3acc3a633f925
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-02 21:26:42 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -22,13 +22,6 @@ class Grupo:
         return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
